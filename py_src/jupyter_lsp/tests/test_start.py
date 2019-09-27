731c3df879ae7a5e0804b813b2d5c25b4280d80c
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-09-26 23:42:07 -0400

    don't test actual proc for now (maybe move into eventloop)

--- py_src/jupyter_lsp/tests/test_start.py
+++ py_src/jupyter_lsp/tests/test_start.py
@@ -1,4 +1,4 @@
-import asyncio
+# import asyncio
 
 from pytest import mark
 
@@ -7,8 +7,3 @@ from pytest import mark
 async def test_start(server_process, unused_tcp_port):
     proc = await server_process([f"--port={unused_tcp_port}"])
     assert proc.returncode is None
-
-    try:
-        await asyncio.wait_for(proc.stdout.readline(), timeout=5.0)
-    except asyncio.TimeoutError:
-        print("timeout!")
