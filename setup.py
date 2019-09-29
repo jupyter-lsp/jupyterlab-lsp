ae7ca4505dfc72822bfdc40e2de6d7d869ed5f04
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-09-29 13:20:26 -0400

    try to support py35

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 import sys
 
 __import__("setuptools").setup(
-    setup_requires=["pytest-runner"] if "test" in sys.argv else []
+    setup_requires=["pytest-runner"] if "test" in sys.argv else [],
+    # py35 apparently doesn't support putting these in setup.cfg
+    data_files=[
+        (
+            "etc/jupyter/jupyter_notebook_config.d",
+            ["py_src/jupyter_lsp/etc/jupyter-lsp-serverextension.json"],
+        )
+    ],
 )
