b334ee4d08c37926c24618e9f8ff2ea93faeb3bb
krassowski <krassowski.michal@gmail.com>
2019-10-01 15:50:42 +0100

    Set repos conditionally, so that MRAN repos are not overwritten in binder

--- install_r_server.sh
+++ install_r_server.sh
@@ -1,2 +1,5 @@
 mkdir -p "$HOME/lib/R/site-library"
-Rscript -e "install.packages('languageserver', repos='http://cran.r-project.org', lib='$HOME/lib/R/site-library')"
+if [ "$TRAVIS" == true ]; then
+  echo "options(repos='http://cran.r-project.org')" > .Rprofile
+fi
+Rscript -e "install.packages('languageserver', lib='$HOME/lib/R/site-library')"
