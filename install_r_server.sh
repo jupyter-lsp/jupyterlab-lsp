ca82e73e1ca24be8ef766b719256fa2117fe5931
krassowski <krassowski.michal@gmail.com>
2019-10-02 11:39:09 +0100

    Back down from the Travis installation, focus on binder - remove sudo

--- install_r_server.sh
+++ install_r_server.sh
@@ -4,5 +4,5 @@ if [ "$TRAVIS" == true ]; then
 fi
 # Rscript -e "install.packages('languageserver', lib='$HOME/lib/R/site-library')"
 # use the master for now (waiting for a release which will include https://github.com/REditorSupport/languageserver/issues/77)
-sudo Rscript -e "install.packages('remotes')"
-sudo Rscript -e "remotes::install_github('REditorSupport/languageserver@f0ec93e64109988e73c3a46fe04e3fdffd2547d3', upgrade='never')"
+Rscript -e "install.packages('remotes')"
+Rscript -e "remotes::install_github('REditorSupport/languageserver@f0ec93e64109988e73c3a46fe04e3fdffd2547d3', upgrade='never')"
