911495153306bc2eba343c9d5c0bfdfc38006904
krassowski <krassowski.michal@gmail.com>
2019-10-02 11:44:26 +0100

    Set a mirror for CRAN

--- install_r_server.sh
+++ install_r_server.sh
@@ -1,7 +1,5 @@
 mkdir -p "$HOME/lib/R/site-library"
-if [ "$TRAVIS" == true ]; then
-  echo "options(repos='http://cran.r-project.org')" > .Rprofile
-fi
+echo "options(repos='http://cran.r-project.org')" > .Rprofile
 # Rscript -e "install.packages('languageserver', lib='$HOME/lib/R/site-library')"
 # use the master for now (waiting for a release which will include https://github.com/REditorSupport/languageserver/issues/77)
 Rscript -e "install.packages('remotes')"
