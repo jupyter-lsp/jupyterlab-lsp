22771f0fd7572d85c55582746a682e301732a974
krassowski <krassowski.michal@gmail.com>
2019-10-02 10:50:10 +0100

    Use master R languageserver and try sudo-install

--- install_r_server.sh
+++ install_r_server.sh
@@ -2,4 +2,7 @@ mkdir -p "$HOME/lib/R/site-library"
 if [ "$TRAVIS" == true ]; then
   echo "options(repos='http://cran.r-project.org')" > .Rprofile
 fi
-Rscript -e "install.packages('languageserver', lib='$HOME/lib/R/site-library')"
+# Rscript -e "install.packages('languageserver', lib='$HOME/lib/R/site-library')"
+# use the master for now (waiting for a release which will include https://github.com/REditorSupport/languageserver/issues/77)
+sudo Rscript -e "install.packages('remotes')"
+sudo Rscript -e "remotes::install_github('REditorSupport/languageserver@master', upgrade='never')"
