4a61aebadbbd3fdbdd2bf829275e34ce94b5e01c
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-10-05 14:23:44 -0400

    try string

--- py_src/jupyter_lsp/specs/r_languageserver.py
+++ py_src/jupyter_lsp/specs/r_languageserver.py
@@ -4,5 +4,5 @@ from .utils import HELPERS, ShellSpec
 class RLanguageServer(ShellSpec):
     key = "r-languageserver"
     cmd = "Rscript"
-    args = ["--slave", (HELPERS / "languageserver.R")]
+    args = ["--slave", str(HELPERS / "languageserver.R")]
     languages = ["r"]
