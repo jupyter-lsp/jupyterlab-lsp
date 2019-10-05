a51bc997b54f2454c61e213ea8f03df5cc24de31
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-10-05 14:22:42 -0400

    try without posix path

--- py_src/jupyter_lsp/specs/r_languageserver.py
+++ py_src/jupyter_lsp/specs/r_languageserver.py
@@ -4,5 +4,5 @@ from .utils import HELPERS, ShellSpec
 class RLanguageServer(ShellSpec):
     key = "r-languageserver"
     cmd = "Rscript"
-    args = ["--slave", (HELPERS / "languageserver.R").as_posix()]
+    args = ["--slave", (HELPERS / "languageserver.R")]
     languages = ["r"]
