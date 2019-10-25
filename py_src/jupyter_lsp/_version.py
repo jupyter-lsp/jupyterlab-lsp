7fa0331f0fb3108b064d607e77c3fe916e477e0e
krassowski <krassowski.michal@gmail.com>
2019-10-25 23:52:24 +0100

    Fix description issues which caused PyPI to reject

--- py_src/jupyter_lsp/_version.py
+++ py_src/jupyter_lsp/_version.py
@@ -1,3 +1,3 @@
 """ single source of truth for jupyter_lsp version
 """
-__version__ = "0.5.0"
+__version__ = "0.6.0a1"
--- setup.py
+++ setup.py
@@ -4,11 +4,25 @@ from pathlib import Path
 
 import setuptools
 
+try:
+    from pypandoc import convert
+
+    def get_long_description(file_name):
+        return convert(file_name, 'rst', 'md')
+
+except ImportError:
+
+    def get_long_description(file_name):
+        with open(file_name) as f:
+            return f.read()
+
+
 setuptools.setup(
     version=re.findall(
         r"""__version__ = "([^"]+)"$""",
         (Path(__file__).parent / "py_src" / "jupyter_lsp" / "_version.py").read_text(),
     )[0],
+    long_description=get_long_description('README.md'),
     setup_requires=["pytest-runner"] if "test" in sys.argv else [],
     # py35 apparently doesn't support putting these in setup.cfg
     data_files=[
