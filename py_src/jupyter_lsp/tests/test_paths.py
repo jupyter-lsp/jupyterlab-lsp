e0dc182ae3523b4b60d5c00f2788a73ae95aa8eb
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-11-04 09:31:59 -0500

    more work on paths, invoke platform properly

--- py_src/jupyter_lsp/tests/test_paths.py
+++ py_src/jupyter_lsp/tests/test_paths.py
@@ -5,7 +5,7 @@ import pytest
 
 from ..paths import normalized_uri
 
-WIN = platform.system == "Windows"
+WIN = platform.system() == "Windows"
 HOME = pathlib.Path("~").expanduser()
 
 
@@ -13,16 +13,16 @@ HOME = pathlib.Path("~").expanduser()
 @pytest.mark.parametrize(
     "root_dir, expected_root_uri",
     [
+        ["~", HOME.as_uri()],
         # probably need to try some other things
-        [str(HOME / "foo"), (HOME / "foo").as_uri()]
+        [str(HOME / "foo"), (HOME / "foo").as_uri()],
     ],
 )
 def test_normalize_paths(root_dir, expected_root_uri):  # pragma: no cover
-    """ paths we can easily test on POSIX platforms
-    """
     assert normalized_uri(root_dir) == expected_root_uri
 
 
+@pytest.mark.skipif(~WIN, reason="can't test Windows paths on POSIX")
 @pytest.mark.parametrize(
     "root_dir, expected_root_uri",
     [
@@ -32,6 +32,4 @@ def test_normalize_paths(root_dir, expected_root_uri):  # pragma: no cover
     ],
 )
 def test_normalize_windows_path_case(root_dir, expected_root_uri):  # pragma: no cover
-    """ paths we can easily test on non-windows platforms
-    """
     assert normalized_uri(root_dir) == expected_root_uri
