8fe9a361b4397c40a05b8dfadc2b72f7417d8be8
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-11-04 09:53:35 -0500

    some work on path test/coverage

--- py_src/jupyter_lsp/tests/test_paths.py
+++ py_src/jupyter_lsp/tests/test_paths.py
@@ -11,17 +11,24 @@ HOME = pathlib.Path("~").expanduser()
 PY35 = sys.version_info[:2] < (3, 5)
 
 
+@pytest.mark.skipif(WIN, reason="can't test POSIX paths on Windows")
+@pytest.mark.parametrize("root_dir, expected_root_uri", [["~", HOME.as_uri()]])
+def test_normalize_posix_path_home(root_dir, expected_root_uri):  # pragma: no cover
+    assert normalized_uri(root_dir) == expected_root_uri
+
+
 @pytest.mark.skipif(PY35, reason="can't test non-existant paths on py35")
 @pytest.mark.skipif(WIN, reason="can't test POSIX paths on Windows")
 @pytest.mark.parametrize(
     "root_dir, expected_root_uri",
     [
-        ["~", HOME.as_uri()],
         # probably need to try some other things
-        [str(HOME / "foo"), (HOME / "foo").as_uri()],
+        [str(HOME / "foo"), (HOME / "foo").as_uri()]
     ],
 )
-def test_normalize_paths(root_dir, expected_root_uri):  # pragma: no cover
+def test_normalize_posix_path_home_subdir(
+    root_dir, expected_root_uri
+):  # pragma: no cover
     assert normalized_uri(root_dir) == expected_root_uri
 
 
