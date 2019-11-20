ce069b2a1ba6b1703bb090c2ca185e4bdf8e4d61
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-11-19 23:28:20 -0500

    bump to jupyterlab 1.2 (still test 1.1)

--- ci/env_template.py
+++ ci/env_template.py
@@ -2,5 +2,8 @@ from pathlib import Path
 import sys
 
 Path("..", "env-test.yml").write_text(
-    Path("env-test.yml.in").read_text().format(sys.argv[1])
+    Path("env-test.yml.in").read_text().format(
+        python=sys.argv[1],
+        lab=sys.argv[2]
+    )
 )
