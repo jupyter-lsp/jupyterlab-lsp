8d6da203724016ca11c4277da721ea0b30573ce5
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-01-12 00:19:48 -0500

    fix some lab versions, more windows skipping

--- scripts/utest.py
+++ scripts/utest.py
@@ -1,8 +1,17 @@
 """ run python unit tests with pytest
 """
+import platform
 import sys
 
 import pytest
+
+OS = platform.system()
+PY = "".join(map(str, sys.version_info[:2]))
+
+OS_PY_ARGS = {
+    # notebook and ipykernel releases do not yet support python 3.8 on windows
+    ("Windows", "38"): ["-k", "not serverextension"]
+}
 
 
 def run_tests():
@@ -20,6 +29,7 @@ def run_tests():
         "--flake8",
         "--cov-fail-under=100",
         "-vv",
+        *OS_PY_ARGS.get((OS, PY), []),
     ] + list(sys.argv[1:])
 
     return pytest.main(args)
