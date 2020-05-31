5cf3b81dfb1c6322919c6dabbf1b7a7c79f5a3da
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-05-31 19:59:16 -0400

    linting

--- packages/jupyterlab-lsp/src/manager.ts
+++ packages/jupyterlab-lsp/src/manager.ts
@@ -16,164 +16,153 @@ const CONTROL_COMM_TARGET = 'jupyter.lsp.control';
 const SERVER_COMM_TARGET = 'jupyter.lsp.server';
 
 export class LanguageServerManager implements ILanguageServerManager {
-                protected _sessionsChanged: Signal<
-           ILanguageServerManager,
-           void
-         > = new Signal<ILanguageServerManager, void
-      >(this);
-                protected _sessions: TSessionMap = new Map();
-                private _settings: ServerConnection.ISettings;
-                private _baseUrl: string;
-                       private _serviceManager: ServiceManager;
-         private _kernelSessionConnection: ISessionConnection;
+  protected _sessionsChanged: Signal<ILanguageServerManager, void> = new Signal<
+    ILanguageServerManager,
+    void
+  >(this);
+  protected _sessions: TSessionMap = new Map();
+  private _settings: ServerConnection.ISettings;
+  private _baseUrl: string;
+  private _serviceManager: ServiceManager;
+  private _kernelSessionConnection: ISessionConnection;
 
-                constructor(options: ILanguageServerManager.IOptions) {
-                  this._settings = options.settings || ServerConnection.makeSettings();
-                  this._baseUrl = options.baseUrl || PageConfig.getBaseUrl();
-                  this._serviceManager = options.serviceManager;
-                  this.fetchSessions().catch(console.warn);
-                                this.initKernel().catch(console.warn);
-         }
+  constructor(options: ILanguageServerManager.IOptions) {
+    this._settings = options.settings || ServerConnection.makeSettings();
+    this._baseUrl = options.baseUrl || PageConfig.getBaseUrl();
+    this._serviceManager = options.serviceManager;
+    this.fetchSessions().catch(console.warn);
+    this.initKernel().catch(console.warn);
+  }
 
-         get statusUrl() {
-                  return URLExt.join(
-             this._baseUrl,
-             ILanguageServerManager.URL_NS,
-             'status'
+  get statusUrl() {
+    return URLExt.join(this._baseUrl, ILanguageServerManager.URL_NS, 'status');
+  }
 
-                         );
-                }
+  get sessionsChanged() {
+    return this._sessionsChanged;
+  }
 
-                get sessionsChanged() {
-                  return this._sessionsChanged;
-         }
+  get sessions(): TSessionMap {
+    return this._sessions;
+  }
 
-         get sessions(): TSessionMap {
-                  return this._sessions;
-                }
+  getServerId(options: ILanguageServerManager.IGetServerIdOptions) {
+    // most things speak language
+    for (const [key, session] of this._sessions.entries()) {
+      if (options.language) {
+        if (session.spec.languages.indexOf(options.language) !== -1) {
+          return key;
+        }
+      }
+    }
+    return null;
+  }
 
-                getServerId(options: ILanguageServerManager.IGetServerIdOptions) {
-                  // most things speak language
-                  for (const [key, session] of this._sessions.entries()) {
-                    if (options.language) {
-                      if (session.spec.languages.indexOf(options.language) !== -1) {
-                        return key;
-               }
-                    }
-                       }
-                  return null;
-                       }
+  async fetchSessions() {
+    let response = await ServerConnection.makeRequest(
+      this.statusUrl,
+      { method: 'GET' },
+      this._settings
+    );
 
-                       async fetchSessions() {
-                                let response = await ServerConnection.makeRequest(
-                    this.statusUrl,
-                    { method: 'GET' },
-                           this._settings
-           );
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
 
-           if (!response.ok) {
-             throw new Error(response.statusText);
-                  }
+    let sessions: SCHEMA.Sessions;
 
-           let sessions: SCHEMA.Sessions;
+    try {
+      sessions = (await response.json()).sessions;
+    } catch (err) {
+      console.warn(err);
+      return;
+    }
 
-                  try {
-                    sessions = (await response.json()).sessions;
-           } catch (err) {
-                           console.warn(err);
-             return;
-                         }
+    for (const key of Object.keys(sessions)) {
+      if (this._sessions.has(key)) {
+        Object.assign(this._sessions.get(key), sessions[key]);
+      } else {
+        this._sessions.set(key, sessions[key]);
+      }
+    }
 
-                  for (const key of Object.keys(sessions)) {
-                   if  (this._sessions.has(key)) {
-                      Object.assign(this._sessions.get(key), sessions[key]);
-                    } else {
-                             this._sessions.set(key, sessions[key]);
-             }
-           }
+    const oldKeys = this._sessions.keys();
 
-           const oldKeys = this._sessions.keys();
+    for (const oldKey in oldKeys) {
+      if (!sessions[oldKey]) {
+        this._sessions.delete(oldKey);
+      }
+    }
 
-           for (const oldKey in oldKeys) {
-             if (!sessions[oldKey]) {
-               this._sessions.delete(oldKey
-             );
-                           }
-           }
+    this._sessionsChanged.emit(void 0);
+  }
 
-           this._sessionsChanged.emit(void 0);
-         }
+  /**
+   * Register a new kernel
+   */
+  protected _handleKernelChanged({
+    oldValue,
+    newValue
+  }: ISessionConnection.IKernelChangedArgs): void {
+    if (oldValue) {
+      oldValue.removeCommTarget(SERVER_COMM_TARGET, this._handleServerCommOpen);
+    }
 
-         /**
-          * Register a new kernel
-          */
-         protected _handleKernelChanged({
-           oldValue,
-                  newValue
-         }: ISessionConnection.IKernelChangedArgs): void {
-           if (oldValue) {
-             oldValue.removeCommTarget(
-               SERVER_COMM_TARGET,
-               this._handleServerCommOpen
+    if (newValue) {
+      newValue.registerCommTarget(
+        SERVER_COMM_TARGET,
+        this._handleServerCommOpen
+      );
+      console.warn('server comm registered');
+    }
+  }
 
-                    );
-                         }
+  _handleServerCommOpen(comm: IComm, msg: KernelMessage.ICommOpenMsg) {
+    console.warn('server comm openened', comm, msg);
+  }
 
-                  if (newValue) {
-             newValue.registerCommTarget(
-               SERVER_COMM_TARGET,
-               this._handleServerCommOpen
-             );
-             console.warn('server comm registered');
-                         }
-                              }
+  async initKernel() {
+    if (this._kernelSessionConnection) {
+      this._kernelSessionConnection.dispose();
+      this._kernelSessionConnection = null;
+    }
+    await this._serviceManager.ready;
 
-                       _handleServerCommOpen(comm: IComm, msg: KernelMessage.ICommOpenMsg) {
-           console.warn('server comm openened', comm, msg);
-         }
+    const session = (this._kernelSessionConnection = await this._serviceManager.sessions.startNew(
+      {
+        path: '/',
+        type: '',
+        name: 'language-server',
+        kernel: { name: 'jupyter-lsp-kernel' }
+      }
+    ));
 
-         async initKernel() {
-           if (this._kernelSessionConnection) {
-                    this._kernelSessionConnection.dispose();
-             this._kernelSessionConnection = null;
-           }
-           await this._serviceManager.ready;
+    session.kernelChanged.connect((sender, args) => {
+      this._handleKernelChanged(args);
+    });
 
-                         const session = (       this._kernelSessionConnection = await this._serviceManager.sessions.startNew(
-             {
-               path: '/',
-               type: '',
-               name: 'language-server',
-               kernel: { name: 'jupyter-lsp-kernel' }
-             }
-                  ));
+    const { kernel } = session;
 
-           session.kernelChanged.connect((sender, args) => {
-             this._handleKernelChanged(args);
-           });
+    this._handleKernelChanged({
+      name: 'kernel',
+      oldValue: null,
+      newValue: kernel
+    });
 
-           const { kernel } = session;
+    const controlComm = session.kernel.createComm(CONTROL_COMM_TARGET);
 
-                  this._handleKernelChanged({
-             name: 'kernel',
-             oldValue: null,
-             newValue: kernel
-           });
+    controlComm.onMsg = (msg: KernelMessage.ICommMsgMsg) => {
+      console.log('we got a control message', controlComm, msg);
+    };
 
-           const controlComm = session.kernel.createComm(CONTROL_COMM_TARGET);
+    const opened = controlComm.open({ 1: 2 }, { 3: 4 });
+    await opened.done;
 
-           controlComm.onMsg = (msg: KernelMessage.ICommMsgMsg) => {
-             console.log('we got a control message', controlComm, msg);
-           };
+    const future = controlComm.send({ foo: 'bar' }, { baz: 'boo' });
+    await future.done;
 
-           const opened = controlComm.open({ 1: 2 }, { 3: 4 });
-           await opened.done;
+    console.warn('sent a control message');
 
-           const future = controlComm.send({ foo: 'bar' }, { baz: 'boo' });
-           await future.done;
-
-           console.warn('sent a control message');
-
-           // console.warn('comm', comm, future);
-         }
-       }
+    // console.warn('comm', comm, future);
+  }
+}
