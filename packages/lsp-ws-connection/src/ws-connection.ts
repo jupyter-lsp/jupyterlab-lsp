13ab01f9558eb7197bf7867d7be2563dc423ef67
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-06-10 00:35:29 -0400

    replace tslint with eslint

--- packages/lsp-ws-connection/src/ws-connection.ts
+++ packages/lsp-ws-connection/src/ws-connection.ts
@@ -342,7 +342,7 @@ export class LspWsConnection extends events.EventEmitter
     if (!this.isReady) {
       return;
     }
-    this.connection
+    void this.connection
       .sendRequest<protocol.CompletionItem>(
         'completionItem/resolve',
         completionItem
@@ -504,7 +504,7 @@ export class LspWsConnection extends events.EventEmitter
       return;
     }
 
-    this.connection
+    void this.connection
       .sendRequest<Location | Location[] | LocationLink[]>(
         'textDocument/implementation',
         {
