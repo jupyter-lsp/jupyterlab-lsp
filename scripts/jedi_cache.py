05923f751d247321f5139413a9c12ce17433bf93
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-06-21 23:13:17 -0400

    move up jedi script, print out more env and versions

--- scripts/jedi_cache.py
+++ scripts/jedi_cache.py
@@ -1,5 +1,6 @@
 """ utility script to warm up/validate the jedi cache
 """
+import os
 import pathlib
 import shutil
 import sys
@@ -8,39 +9,73 @@ import time
 import IPython
 import jedi
 import jupyterlab
+import notebook
+import parso
+import pyls
 
 SOURCE_TEMPLATE = """
 import {module}
 {module}."""
 
 MODULES_TO_CACHE = [
-    "sys",
     "itertools",
-    "IPython",
     "statistics",
-    "os",
-    "time",
     *sys.modules,
 ]
 
 
 def warm_up_one(module):
+    print(module, end="\t")
     start = time.time()
     script = jedi.Script(SOURCE_TEMPLATE.format(module=module))
     completions = len(script.complete(3, len("{}.".format(module))))
     end = time.time()
-    print(module, completions, end - start)
+    print("\t", completions, end - start)
+
+
+def print_versions():
+    print(".".join(map(str, sys.version_info[:3])), "\t", "python")
+    print(IPython.__version__, "\t", "ipython")
+    print(jedi.__version__, "\t", "jedi")
+    print(jupyterlab.__version__, "\t", "jupyterlab")
+    print(notebook.__version__, "\t", "notebook")
+    print(parso.__version__, "\t", "parso")
+    print(pyls.__version__, "\t", "pyls")
+
+
+def print_env():
+    [
+        print(key, "\t", value)
+        for key, value in sorted(os.environ.items())
+        if "CONDA" in key
+    ]
+
+
+def setup_jedi():
+    print("default jedi environment", jedi.api.environment.get_default_environment())
+    jedi_cache = pathlib.Path(
+        jedi.settings.cache_directory, environment=jedi.InterpreterEnvironment()
+    )
+    if jedi_cache.exists():
+        shutil.rmtree(jedi_cache)
+        print("removed jedi cache!")
+    else:
+        print("no jedi cache was found!")
+
+
+def warm_up(modules):
+    print_env()
+    print("-" * 80)
+    print_versions()
+    print("-" * 80)
+    setup_jedi()
+    print("-" * 80)
+    start = time.time()
+    [warm_up_one(module) for module in modules]
+    end = time.time()
+    print("-" * 80)
+    print(len(modules), "modules in", jedi.settings.cache_directory, end - start)
 
 
 if __name__ == "__main__":
-    jedi_cache = pathlib.Path(jedi.settings.cache_directory)
-    if jedi_cache.exists():
-        shutil.rmtree(jedi_cache)
-    print(IPython.__version__)
-    print(jupyterlab.__version__)
-    start = time.time()
-    modules = sorted(set(sys.argv[1:] or MODULES_TO_CACHE))
-    [warm_up_one(module) for module in modules]
-    end = time.time()
-    print("------------------")
-    print(len(modules), "modules in", jedi.settings.cache_directory, end - start)
+    warm_up(sorted(set(sys.argv[1:] or MODULES_TO_CACHE)))
