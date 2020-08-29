ab1f8e57a98c79f641b6dcbd87cd3373c3530a80
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-08-29 00:41:26 -0400

    work on license url

--- packages/jupyterlab-lsp/src/features/completion/config.tsx
+++ packages/jupyterlab-lsp/src/features/completion/config.tsx
@@ -3,11 +3,16 @@ import * as React from 'react';
 import { LabIcon } from '@jupyterlab/ui-components';
 import { VDomRenderer, VDomModel } from '@jupyterlab/apputils';
 
-import { ILSPCompletionThemeManager } from '@krassowski/completion-theme/lib/types';
+import {
+  ILSPCompletionThemeManager,
+  ICompletionTheme,
+  ILicenseInfo
+} from '@krassowski/completion-theme/lib/types';
 
 import '../../../style/config/completion.css';
 
 type TThemeKindIcons = Map<string, LabIcon>;
+type TThemeMap = Map<string, ICompletionTheme>;
 
 const CONFIG_CLASS = 'jp-LSPCompletion-Config';
 
@@ -18,20 +23,21 @@ export class Configurer extends VDomRenderer<Configurer.Model> {
   };
 
   protected render() {
-    const { theme_ids, kinds, icons } = this.model;
+    const { theme_ids, kinds, icons, themes } = this.model;
     const currentThemeId = this.model.iconsThemeManager.get_current_theme_id();
 
     this.addClass(CONFIG_CLASS);
+    this.addClass('jp-RenderedHTMLCommon');
 
     return (
-      <div className={`jp-RenderedHTMLCommon`}>
+      <div>
         <header>
           <h1>Code Completion Settings</h1>
-          <blockquote>
-            Pick an icon theme to use for symbol references, such as completion
-            hints.
-          </blockquote>
-          <h2>Icon Color Scheme</h2>
+        </header>
+        <section>
+          <h2>
+            <i>TBD: Icon Color Scheme</i>
+          </h2>
           <blockquote>Pick an icon color scheme</blockquote>
           <ul>
             {['colorful', 'monochrome'].map(v => (
@@ -43,37 +49,64 @@ export class Configurer extends VDomRenderer<Configurer.Model> {
               </li>
             ))}
           </ul>
-        </header>
-        <table>
-          <thead>
-            <tr>
-              <th>Current Theme</th>
-              {theme_ids.map((id, i) => (
-                <th key={i}>
-                  <input
-                    type="radio"
-                    defaultValue={id}
-                    name="current-theme"
-                    checked={id === currentThemeId}
-                    onChange={this.onThemeChanged}
-                  />
-                </th>
-              ))}
-            </tr>
-            <tr>
-              <th>Theme Name</th>
-              {theme_ids.map((id, i) => (
-                <th key={i}>
-                  <code>{id}</code>
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {kinds.map(kind => this.renderKind(kind, theme_ids, icons))}
-          </tbody>
-        </table>
+        </section>
+        <section>
+          <h2>
+            Icon Theme <code>{currentThemeId}</code>
+          </h2>
+          <blockquote>
+            Pick an icon theme to use for symbol references, such as completion
+            hints.
+          </blockquote>
+          <table>
+            <thead>
+              <tr>
+                <th>Current Theme</th>
+                {theme_ids.map((id, i) => (
+                  <th key={i}>
+                    <input
+                      type="radio"
+                      defaultValue={id}
+                      name="current-theme"
+                      checked={id === currentThemeId}
+                      onChange={this.onThemeChanged}
+                    />
+                  </th>
+                ))}
+              </tr>
+              <tr>
+                <th>Theme Name</th>
+                {theme_ids.map((id, i) => (
+                  <th key={i}>{themes.get(id).name}</th>
+                ))}
+              </tr>
+              <tr>
+                <th>License</th>
+                {theme_ids.map((id, i) =>
+                  this.renderLicense(themes.get(id).icons.licence, i)
+                )}
+              </tr>
+            </thead>
+            <tbody>
+              {kinds.map(kind => this.renderKind(kind, theme_ids, icons))}
+            </tbody>
+          </table>
+        </section>
       </div>
+    );
+  }
+
+  protected renderLicense(license: ILicenseInfo, key: number) {
+    return (
+      <th key={key}>
+        <a
+          href={license.link}
+          title={`${license.name} by ${license.licensor}`}
+          target="_blank"
+        >
+          {license.abbreviation}
+        </a>
+      </th>
     );
   }
 
@@ -116,9 +149,14 @@ export namespace Configurer {
     _theme_ids: string[];
     _icons: TThemeKindIcons;
     _kinds: string[];
+    _themes: TThemeMap;
 
     get theme_ids() {
       return this._theme_ids;
+    }
+
+    get themes() {
+      return this._themes;
     }
 
     get kinds() {
@@ -144,6 +182,7 @@ export namespace Configurer {
         this._theme_ids = [];
         this._kinds = [];
         this._icons = new Map();
+        this._themes = new Map();
       }
       this.stateChanged.emit(void 0);
     }
@@ -155,9 +194,11 @@ export namespace Configurer {
 
       let icons: TThemeKindIcons = new Map();
       let kinds: string[] = [];
+      const themes: TThemeMap = new Map();
 
       for (const id of theme_ids) {
         const theme = manager.get_theme(id);
+        themes.set(id, theme);
         const theme_icons = manager.get_iconset(theme);
         for (const [kind, icon] of theme_icons.entries()) {
           icons.set(`${kind}-${id}`, icon as LabIcon);
@@ -171,6 +212,7 @@ export namespace Configurer {
       this._theme_ids = theme_ids;
       this._kinds = kinds;
       this._icons = icons;
+      this._themes = themes;
     }
   }
 }
