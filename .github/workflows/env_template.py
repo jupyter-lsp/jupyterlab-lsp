1637ad155e0decc66c6bff713d1e1b9c035e8c97
krassowski <krassowski.michal@gmail.com>
2020-09-08 22:47:02 +0100

    Less templates

--- .github/workflows/env_template.py
+++ .github/workflows/env_template.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import sys
 
-Path("..", "env-test.yml").write_text(
+Path("../..", "env-test.yml").write_text(
     Path("env-test.yml.in").read_text().format(
         lab=sys.argv[1],
         nodejs=sys.argv[2]
