2ecd1b7dbfdf6cb4b4cce476992a92bb110fa28a
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-09-08 09:14:40 -0400

    linting

--- atest/logcheck.py
+++ atest/logcheck.py
@@ -1,5 +1,3 @@
-import os
-
 from bs4 import UnicodeDammit
 
 
@@ -13,12 +11,18 @@ def file_should_not_contain_phrases(filename, offset=0, *phrases):
     try:
         text = raw.decode("utf-8")
     except Exception as err:
-        text = UnicodeDammit.detwingle(raw).decode("utf-8")
+        print("Failed to read", filename, "forcing unicode...\n", err)
+        try:
+            text = UnicodeDammit.detwingle(raw).decode("utf-8")
+        except Exception as err:
+            print("Failed to read", filename, "giving up...\n", err)
+            text = None
 
     matches = {}
 
-    for phrase in phrases:
-        if phrase in text:
-            matches[phrase] = True
+    if text is not None:
+        for phrase in phrases:
+            if phrase in text:
+                matches[phrase] = True
 
-    assert not matches, "Phrases found in {}: {}".format(filename, matches)
+        assert not matches, "Phrases found in {}: {}".format(filename, matches)
