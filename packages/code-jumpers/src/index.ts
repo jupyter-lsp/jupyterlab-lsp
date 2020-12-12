9dc2e1a6032015b19862852b745b84fa41f40563
krassowski <krassowski.michal@gmail.com>
2020-12-12 19:24:32 +0000

    Rename usage, lint

--- packages/code-jumpers/src/index.ts
+++ packages/code-jumpers/src/index.ts
@@ -2,4 +2,4 @@ import { CodeJumper } from './jumpers/jumper';
 import { NotebookJumper } from './jumpers/notebook';
 import { FileEditorJumper } from './jumpers/fileeditor';
 
-export {CodeJumper, NotebookJumper, FileEditorJumper};
+export { CodeJumper, NotebookJumper, FileEditorJumper };
