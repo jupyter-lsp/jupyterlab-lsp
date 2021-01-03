688e682a625215f875972f9d122b11cb781f0443
krassowski <krassowski.michal@gmail.com>
2021-01-03 14:31:30 +0000

    Try to check what's wrong with the docs

--- docs/conf.py
+++ docs/conf.py
@@ -104,4 +104,10 @@ html_context = {
 def setup(app):
     """Runs before the "normal business" of sphinx. Don't go too crazy here."""
     app.add_css_file("css/custom.css")
-    subprocess.check_call(["jlpm", "--ignore-optional"])
+    try:
+        result = subprocess.check_output(["jlpm", "--ignore-optional"])
+        print(result)
+    except subprocess.CalledProcessError as e:
+        print("Returned non-zero exit status:", e.returncode)
+        print(e.output)
+        raise e
