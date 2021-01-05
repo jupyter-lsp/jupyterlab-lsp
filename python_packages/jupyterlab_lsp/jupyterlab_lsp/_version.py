fd2e89034178167d7793b5bfd0263103413b0324
krassowski <krassowski.michal@gmail.com>
2021-01-05 04:48:21 +0000

    Prepare for jupyterlab_lsp rc0

--- python_packages/jupyterlab_lsp/jupyterlab_lsp/_version.py
+++ python_packages/jupyterlab_lsp/jupyterlab_lsp/_version.py
@@ -2,7 +2,9 @@ import json
 
 from ._paths import MAIN_PACKAGE_PATH
 
-__all__ = ["__version__"]
-__version__ = json.loads(
+_js_version = json.loads(
     (MAIN_PACKAGE_PATH / "package.json").read_text(encoding="utf-8")
 )["version"]
+
+__all__ = ["__version__"]
+__version__ = _js_version + "rc0"
