ee69a173d426b0d494303c18ad2e07edff645d3d
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2021-01-30 22:29:36 +0000

    Try to fix tests on MacOS and Windows

--- python_packages/jupyter_lsp/jupyter_lsp/tests/test_stdio.py
+++ python_packages/jupyter_lsp/jupyter_lsp/tests/test_stdio.py
@@ -22,27 +22,27 @@ print()
 
 
 class CommunicatorSpawner:
-    def __init__(self, tmpdir):
-        self.tmpdir = tmpdir
+    def __init__(self, tmp_path):
+        self.tmp_path = tmp_path
 
     def spawn_writer(self, message: str, repeats: int = 1, interval=None):
         length = len(message) * repeats
-        commands_file = self.tmpdir / "writer.py"
+        commands_file = self.tmp_path / "writer.py"
         commands_file.write(
             WRITER_TEMPLATE.format(
                 length=length, repeats=repeats, interval=interval or 0, message=message
             )
         )
         return subprocess.Popen(
-            ["python", "-u", commands_file.realpath()],
+            ["python", "-u", str(commands_file)],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
         )
 
 
 @pytest.fixture
-def communicator_spawner(tmpdir):
-    return CommunicatorSpawner(tmpdir)
+def communicator_spawner(tmp_path):
+    return CommunicatorSpawner(tmp_path)
 
 
 def communicate_and_close(process, wait=1):
@@ -72,7 +72,7 @@ async def test_reader(message, repeats, interval, communicator_spawner):
         message=message, repeats=repeats, interval=interval
     )
     reader = LspStdIoReader(stream=process.stdout, queue=queue)
-    timeout = 2 + (interval or 1) * repeats * 2
+    timeout = 2 + (interval or 1) * repeats * 10
 
     communicate_and_close(process)
     await asyncio.wait_for(reader.read(), timeout=timeout)
