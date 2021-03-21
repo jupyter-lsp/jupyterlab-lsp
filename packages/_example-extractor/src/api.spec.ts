f95d7ba2a183dac4076d6f6cedc8f75f55c978c6
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-03-21 11:03:35 -0400

    clobber some lint

--- packages/_example-extractor/src/api.spec.ts
+++ packages/_example-extractor/src/api.spec.ts
@@ -1,11 +1,27 @@
+import { expect } from 'chai';
+
 import { extractor } from '.';
+
+import { IExtractedCode } from '@krassowski/jupyterlab-lsp';
 
 const EXAMPLE = `%%foo
 bar
 `;
 
-describe('The extractor', () => {
-  describe('extracts', () => {
-    extractor.extract_foreign_code(EXAMPLE);
+const EXPECTED: IExtractedCode = {
+  foreign_code: 'bar',
+  host_code: EXAMPLE,
+  range: { end: { column: 0, line: 2 }, start: { column: 0, line: 1 } },
+  virtual_shift: null
+};
+
+describe('The foo extractor', () => {
+  it('extracts %%foo bar', () => {
+    const extracted = extractor.extract_foreign_code(EXAMPLE);
+    expect(extracted[0]).to.equal(EXPECTED);
+  });
+  it('does not extract baz', () => {
+    const extracted = extractor.extract_foreign_code('baz');
+    expect(extracted).to.equal([]);
   });
 });
