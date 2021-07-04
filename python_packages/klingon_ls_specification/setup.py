61d4e4d9851263286f7369742eb55685f5aa4714
krassowski <krassowski.michal@gmail.com>
2021-07-04 12:47:08 +0100

    Use pre-built extension for Klingon file type

--- python_packages/klingon_ls_specification/setup.py
+++ python_packages/klingon_ls_specification/setup.py
@@ -1,3 +1,24 @@
+from pathlib import Path
+
 from setuptools import setup
 
-setup()
+LABEXTENSIONS_DIR = Path("klingon_ls_specification/labextensions")
+LABEXTENSIONS_INSTALL_DIR = Path("share") / "jupyter" / "labextensions"
+LAB_PACKAGE_PATH = (
+    LABEXTENSIONS_DIR / "@krassowski" / "_klingon-integration" / "package.json"
+)
+
+
+def get_data_files():
+    extension_files = [
+        (
+            str(LABEXTENSIONS_INSTALL_DIR / file.relative_to(LABEXTENSIONS_DIR).parent),
+            [str(file.as_posix())],
+        )
+        for file in LABEXTENSIONS_DIR.rglob("*.*")
+    ]
+
+    return extension_files
+
+
+setup(data_files=get_data_files())
