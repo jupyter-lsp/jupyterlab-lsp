7da93b4fe14f13cf29e782bcf5601a11cc9774ae
Karla Spuldaro <karla.spuldaro@ibm.com>
2021-07-14 18:01:31 -0400

    Lint formatting

--- python_packages/jupyter_lsp/jupyter_lsp/specs/utils.py
+++ python_packages/jupyter_lsp/jupyter_lsp/specs/utils.py
@@ -142,14 +142,10 @@ class NodeModuleSpec(SpecBase):
         spec["troubleshoot"] = "\n\n".join(troubleshooting)
 
         is_installed = self.is_installed(mgr)
-        
+
         return {
             self.key: {
-                "argv": (
-                    [mgr.nodejs, node_module, *self.args]
-                    if is_installed
-                    else []
-                ),
+                "argv": ([mgr.nodejs, node_module, *self.args] if is_installed else []),
                 "languages": self.languages,
                 "version": SPEC_VERSION,
                 **spec,
