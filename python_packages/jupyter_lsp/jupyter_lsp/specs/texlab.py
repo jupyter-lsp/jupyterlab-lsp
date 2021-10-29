e01dd8b22270c68e6daebe4d390fb87671b4b53d
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2021-10-29 18:30:14 +0100

    Document texlab troubleshooting

--- python_packages/jupyter_lsp/jupyter_lsp/specs/texlab.py
+++ python_packages/jupyter_lsp/jupyter_lsp/specs/texlab.py
@@ -1,6 +1,19 @@
 from .config import load_config_schema
 from .utils import ShellSpec
 
+TROUBLESHOOT = """\
+Please ensure that texlab executable is in the PATH; \
+this should happen automatically when installing texlab from Conda, \
+but may require manual configuration of PATH environment variable \
+if you compiled texlab from source.
+
+You can ensure check if texlab is in the PATH, by running:
+
+  which texlab
+
+which should return the path to the executable (if found).
+"""
+
 
 class Texlab(ShellSpec):
     cmd = key = "texlab"
@@ -15,4 +28,5 @@ class Texlab(ShellSpec):
         install=dict(conda="conda install -c conda-forge texlab chktex"),
         config_schema=load_config_schema(key),
         env=dict(RUST_BACKTRACE="1"),
+        troubleshoot=TROUBLESHOOT,
     )
