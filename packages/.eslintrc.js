ece114b5d2ad02310d028f83954afe0a7cc22224
krassowski <5832902+krassowski@users.noreply.github.com>
2023-08-26 20:55:21 +0100

    Enable no-empty-interface rule

--- packages/.eslintrc.js
+++ packages/.eslintrc.js
@@ -58,7 +58,7 @@ module.exports = {
     ],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-empty-interface': 'error',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-floating-promises': ['error', { ignoreVoid: true }],
     '@typescript-eslint/no-inferrable-types': 'off',
