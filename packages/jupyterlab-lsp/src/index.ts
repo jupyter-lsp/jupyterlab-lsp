335a76d365a86da3c57234f093433c27bfd3f158
krassowski <5832902+krassowski@users.noreply.github.com>
2023-09-08 22:12:30 +0100

    Fix `priority` not being respected

--- packages/jupyterlab-lsp/src/index.ts
+++ packages/jupyterlab-lsp/src/index.ts
@@ -156,9 +156,11 @@ export class LSPExtension {
       {}) as TLanguageServerConfigurations;
 
     // Add `configuration` as a copy of `serverSettings` to work with changed name upstream
+    // Add `rank` as a copy of priority fot the same reason.
     languageServerSettings = Object.fromEntries(
       Object.entries(languageServerSettings).map(([key, value]) => {
         value.configuration = value.serverSettings;
+        value.rank = value.priority;
         return [key, value];
       })
     );
