3fb2bdef2ca666a998231c01972102df5bef787f
krassowski <5832902+krassowski@users.noreply.github.com>
2023-09-10 05:14:03 +0100

    Fix defaults population which requires schema to be available,
    and a workaround of sending the update again because the connection
    manager is now a separate plugin which starts and can sent initial
    configuration before our plugin gets to setting transformation.
    
    Also use promise delegate over custom method to simplify code.

--- packages/jupyterlab-lsp/src/settings.ts
+++ packages/jupyterlab-lsp/src/settings.ts
@@ -8,7 +8,8 @@ import { TranslationBundle } from '@jupyterlab/translation';
 import {
   JSONExt,
   ReadonlyPartialJSONObject,
-  ReadonlyJSONObject
+  ReadonlyJSONObject,
+  PromiseDelegate
 } from '@lumino/coreutils';
 import { Signal, ISignal } from '@lumino/signaling';
 import { FieldProps } from '@rjsf/utils';
@@ -145,16 +146,7 @@ export class SettingsSchemaManager {
     this._lastValidation = null;
     this._lastUserServerSettings = null;
     this._lastUserServerSettingsDoted = null;
-    this._defaultsPopulated = this._createDefaultsPromise();
     this._validationErrors = [];
-  }
-
-  private _defaultsPopulated: Promise<void>;
-  private _populatedAccept: (value: unknown) => void;
-  private _createDefaultsPromise(): Promise<void> {
-    return new Promise(accept => {
-      this._populatedAccept = accept;
-    });
   }
 
   get schemaValidated(): ISignal<
@@ -175,6 +167,9 @@ export class SettingsSchemaManager {
    */
   async setupSchemaTransform(pluginId: string): Promise<void> {
     const languageServerManager = this.options.languageServerManager;
+    // To populate defaults we require specs to be available, so we need to
+    // wait for until after the `languageServerManager` is ready.
+    await languageServerManager.ready;
 
     // Transform the plugin object to return different schema than the default.
     this.options.settingRegistry.transform(pluginId, {
@@ -200,7 +195,7 @@ export class SettingsSchemaManager {
         if (!this._canonical) {
           this._canonical = JSONExt.deepCopy(plugin.schema);
           this._populate(plugin, this._canonical);
-          this._populatedAccept(void 0);
+          this._defaultsPopulated.resolve(void 0);
         }
 
         return {
@@ -219,7 +214,7 @@ export class SettingsSchemaManager {
     // race condition, see https://github.com/jupyterlab/jupyterlab/issues/12978
     languageServerManager.sessionsChanged.connect(async () => {
       this._canonical = null;
-      this._defaultsPopulated = this._createDefaultsPromise();
+      this._defaultsPopulated = new PromiseDelegate();
       await this.options.settingRegistry.reload(pluginId);
     });
   }
@@ -272,7 +267,7 @@ export class SettingsSchemaManager {
       }
       if (!configSchema.properties) {
         this.console.warn(
-          'No properites in config schema - skipping transformation for',
+          'No properties in config schema - skipping transformation for',
           serverKey
         );
         continue;
@@ -373,10 +368,13 @@ export class SettingsSchemaManager {
     this._defaults = defaults;
   }
 
+  /**
+   * Normalize settings by dotted and nested specs, and merging with defaults.
+   */
   async normalizeSettings(
     composite: Required<LanguageServers>
   ): Promise<Required<LanguageServers>> {
-    await this._defaultsPopulated;
+    await this._defaultsPopulated.promise;
     // Cache collapsed settings for speed and to only show dialog once.
     // Note that JupyterLab attempts to transform in "preload" step (before splash screen end)
     // and then again for deferred extensions if the initial transform in preload timed out.
@@ -583,6 +581,7 @@ export class SettingsSchemaManager {
   }
 
   private _defaults: LanguageServerSettings;
+  private _defaultsPopulated = new PromiseDelegate();
   private _validationErrors: ISchemaValidator.IError[];
   private _schemaValidated: Signal<
     SettingsSchemaManager,
