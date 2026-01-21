feac3521dddbee00df474ce79ae50b837a832c30
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2026-01-21 12:30:09 +0000

    Fix typing/imports

--- packages/jupyterlab-lsp/src/context.ts
+++ packages/jupyterlab-lsp/src/context.ts
@@ -1,4 +1,4 @@
-import type { EditorView } from '@codemirror/view';
+import { EditorView } from '@codemirror/view';
 import { JupyterFrontEnd } from '@jupyterlab/application';
 import { CodeEditor } from '@jupyterlab/codeeditor';
 import type { CodeMirrorEditor } from '@jupyterlab/codemirror';
@@ -231,7 +231,7 @@ export class ContextAssembler {
     if (!cmContent) {
       return;
     }
-    const cmView = EditorView.findFromDOM(cmContent);
+    const cmView = EditorView.findFromDOM(cmContent as HTMLElement);
 
     if (!cmView) {
       return;
