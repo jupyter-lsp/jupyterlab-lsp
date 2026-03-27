180251daab03bd5d1bd881f23297ecd13393aaa9
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2026-03-27 20:06:05 +0000

    fix syntax

--- atest/_libraries/diagnostics.py
+++ atest/_libraries/diagnostics.py
@@ -18,7 +18,7 @@ def page_contains_diagnostic(driver: WebDriver, selector, negate=False):
     driver.execute_script(
         """
     arguments[0].map(el => {
-      let view = el.cmView | el.cmTile;
+      let view = el.cmView || el.cmTile;
       let diagnostic = view.mark.spec.diagnostic;
       el.title = diagnostic.message + " (" + diagnostic.source + ")";
     });
